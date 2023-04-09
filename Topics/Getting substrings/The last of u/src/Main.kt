afdf1bc2254fc1459e6dc5be7b94fd5dd189e21f
Epap <peia@inbox.ru>
2023-04-09 11:21:01 +0300

    Program that capitalizes all letters after the last u Another variant

--- Topics/Getting substrings/The last of u/src/Main.kt
+++ Topics/Getting substrings/The last of u/src/Main.kt
@@ -1,11 +1,4 @@
-fun capitalizeAfterLastU(a: String) = if (a.lastIndexOf('u') == -1) {
-    a
-} else {
-    a.substringBeforeLast('u') + 'u' +
-            a.substringAfterLast('u').uppercase()
-}
-
 fun main() {
     val string = readln()
-    println(capitalizeAfterLastU(string))
+    println(string.replaceAfterLast('u', string.substringAfterLast('u', "").uppercase()))
 }
